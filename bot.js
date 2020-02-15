40493bdc09b475c6ad1447875247311cd7cda5e8
BesoAl <60975137+BesoAl@users.noreply.github.com>
2020-02-15 04:23:33 +0200

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,13 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+console.log("Scrpit By Dream");
+
+
+client.on("ready", () => {
+let channel =     client.channels.get("677162453297004610")
+setInterval(function() {
+channel.send)(`SPAM F1 SPAM AR`)
+}, 30)
+})
+
+client.login(process.env.BOT_TOKEN);
